430c3feba1f3dc034c41218d85c3088d59db22ff
duc <1025434218@qq.com>
2018-10-14 17:12:53 +0800

    前台elasticsearch

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ $router->get('/articles/{id}', function ($id, Trending $trending, ArticleRepoImp
 });
 
 $router->get('/articles', function () {
-    return ArticleResource::collection(Article::with('author')->latest()->select(['id', 'category_id', 'head_image', 'title', 'created_at', 'author_id'])->paginate());
+    return ArticleResource::collection(Article::with('author')->latest()->select(['id', 'category_id', 'head_image', 'title', 'desc', 'created_at', 'author_id'])->paginate());
 });
 
 $router->get('/search_articles', function (Request $request) {
