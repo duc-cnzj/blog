64f97dc1b286f9dcc60efa9602217564f9af197f
duc <1025434218@qq.com>
2018-10-27 17:17:20 +0800

    f

--- illuminate/ModelMakeCommand.php
+++ illuminate/ModelMakeCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Str;
 use Illuminate\Console\GeneratorCommand;
 use Symfony\Component\Console\Input\InputOption;
 
-class ConsoleMakeCommand extends GeneratorCommand
+class ModelMakeCommand extends GeneratorCommand
 {
     /**
      * The console command name.
