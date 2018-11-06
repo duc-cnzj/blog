80b9acd70760547a002608e84d8bf4d3b41adc84
duc <1025434218@qq.com>
2018-11-06 09:49:41 +0800

    数据库备份

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\RefreshImportCommand;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
@@ -25,6 +26,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $enabled = env('BACKUP_ENABLED', false);
+
+        if ($enabled) {
+            // 数据备份
+            $olders = Storage::files('backups');
+            $last = array_pop($olders); // 最新的那份
+            Storage::delete($olders); // 删除旧的备份文件
+
+            $schedule->command("db:backup --database=mysql --destination=local --destinationPath=backups/ --timestamp='Y_m_d_H_i_s' --compression=gzip
+            ")->everyMinute();
+
+            // 备份数据恢复
+            // $lastDumpFile = array_last(Illuminate\Support\Facades\Storage::files('backups'));
+
+            // $schedule->command("db:restore --database=mysql --compression=gzip --source=local --sourcePath=/backups/{$lastDumpFile}
+            // ")->everyMinute();
+        }
     }
 }
