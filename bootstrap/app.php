12df5ec603c82123e636b0f1e8bcc85e9ec77287
duc <1025434218@qq.com>
2018-11-28 11:18:32 +0800

    文章广播时间测试 && 文章删除缓存测试

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -130,6 +130,7 @@ $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
     require __DIR__ . '/../routes/web.php';
+    require __DIR__ . '/../routes/admin.php';
 });
 
 return $app;
