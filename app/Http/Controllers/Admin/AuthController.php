5279c6b4474041b09f954c6cdd5fb1cb8df61acb
duc <1025434218@qq.com>
2018-12-01 07:41:28 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -93,7 +93,7 @@ class AuthController extends Controller
                 'access_token' => $token,
                 'token_type'   => 'bearer',
                 'expires_in'   => \Auth::factory()->getTTL() * 60,
-            ]
+            ],
         ]);
     }
 
