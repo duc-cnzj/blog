5f2d51ea8f0c118735f1110e3fe50abcd2e05206
duc <1025434218@qq.com>
2018-12-03 11:43:30 +0800

    前端文件打包

--- shell/migrate.sh
+++ shell/migrate.sh
@@ -12,8 +12,6 @@ echo "----------------- 数据库创建 done -------------------"
 
 echo "为了确保 ElasticSearch 索引创建成功，请等待 15s"
 
-sleep 15
-
 echo "-------------- elastic 数据索引创建 ----------------"
 php artisan elastic:create-index App\\ES\\ArticleIndexConfigurator > /dev/null 2>&1
 php artisan scout:import "App\Article" > /dev/null 2>&1
