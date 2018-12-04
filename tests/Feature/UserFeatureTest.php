56fab358600b22c8b92f6a177e5fcbb5abb2e3e3
duc <1025434218@qq.com>
2018-12-04 14:49:27 +0800

    test

---
+++ tests/Feature/UserFeatureTest.php
@@ -0,0 +1,88 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class UserFeatureTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function only_admin_can_update_other_user_profile()
+    {
+        $user = $this->signIn(['name'=>'admin']);
+        $user2 = create(\App\User::class, [
+            'name' => 'user2',
+            'email' => '2@q.com',
+        ]);
+
+        $this->json('PUT', "/admin/users/{$user2->id}", [
+           'name' => 'user3',
+           'email' => '1234..', // fail
+        ])->seeStatusCode(422);
+
+        $this->json('PUT', "/admin/users/{$user2->id}", [
+           'name' => 'user3',
+           'email' => '7@q.com', // fail
+        ])->seeStatusCode(204);
+        $this->assertEquals('user3', $user2->fresh()->name);
+        $this->assertEquals('7@q.com', $user2->fresh()->email);
+
+        $this->actingAs($user2, 'api');
+        $this->json('PUT', "/admin/users/{$user->id}", [
+            'name' => 'admin1',
+        ])->seeStatusCode(403);
+        $this->assertEquals('admin', $user->fresh()->name);
+    }
+
+    /** @test */
+    public function only_admin_can_delete_any_user_except_admin()
+    {
+        $user = $this->signIn();
+        $user2 = create(\App\User::class);
+        $user3 = create(\App\User::class);
+        $this->assertEquals(3, \App\User::count());
+        $this->json('DELETE', "/admin/users/{$user->id}")
+            ->seeStatusCode(403)
+            ->seeJsonContains(['message' => '超级管理员不能删除']);
+
+        $this->json('DELETE', "/admin/users/{$user2->id}")->seeStatusCode(204);
+        $this->assertEquals(2, \App\User::count());
+
+        $this->actingAs($user3, 'api');
+        $this->json('DELETE', "/admin/users/{$user->id}")->seeStatusCode(403);
+        $this->assertEquals(2, \App\User::count());
+    }
+
+    /** @test */
+    public function when_user_deleted_all_articles_also_deleted()
+    {
+        $user = create(\App\User::class);
+        create(\App\Article::class, ['author_id' => $user->id], 5);
+
+        $this->assertEquals(5, $user->articles()->count());
+        $user->delete();
+        $this->assertEquals(0, \App\Article::count());
+    }
+
+    /** @test */
+    public function when_user_deleted_all_categories_set_zero()
+    {
+        $user = create(\App\User::class);
+        create(\App\Category::class, ['user_id' => $user->id], 5);
+
+        $this->assertEquals(5, $user->categories()->count());
+        $user->delete();
+        $this->assertEquals(0, \App\Category::where('user_id', $user->id)->count());
+    }
+
+    /** @test */
+    public function when_user_deleted_all_tags_set_zero()
+    {
+        $user = create(\App\User::class);
+        create(\App\Tag::class, ['user_id' => $user->id], 5);
+
+        $this->assertEquals(5, $user->tags()->count());
+        $user->delete();
+        $this->assertEquals(0, \App\Tag::where('user_id', $user->id)->count());
+    }
+}
