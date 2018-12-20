fa8db94bed15b6830c039c5b8ecfd05fd10cf5fa
duc <1025434218@qq.com>
2018-12-20 13:00:43 +0000

    Apply fixes from StyleCI

--- config/services.php
+++ config/services.php
@@ -15,13 +15,13 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
     'ses' => [
-        'key' => env('SES_KEY'),
+        'key'    => env('SES_KEY'),
         'secret' => env('SES_SECRET'),
         'region' => env('SES_REGION', 'us-east-1'),
     ],
@@ -31,18 +31,18 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'model'   => App\User::class,
+        'key'     => env('STRIPE_KEY'),
+        'secret'  => env('STRIPE_SECRET'),
         'webhook' => [
-            'secret' => env('STRIPE_WEBHOOK_SECRET'),
+            'secret'    => env('STRIPE_WEBHOOK_SECRET'),
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
     ],
 
     'github' => [
-        'client_id' => env('GITHUB_CLIENT_ID', '8fd6f41dcfc1001089c7'),         // 你的 GitHub Client ID
+        'client_id'     => env('GITHUB_CLIENT_ID', '8fd6f41dcfc1001089c7'),         // 你的 GitHub Client ID
         'client_secret' => env('GITHUB_CLIENT_SECRET', '3ea90857a5524407654ed9ddcceacf82a429e9ad'), // 你的 GitHub Client Secret
-        'redirect' => env('GITHUB_REDIRECT', 'http://duc.free.idcfengye.com/login/github/callback'),
+        'redirect'      => env('GITHUB_REDIRECT', 'http://duc.free.idcfengye.com/login/github/callback'),
     ],
 ];
